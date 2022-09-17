cef91986a5f2ce76a5bef2747e69b85552cbc9bd
Ryan Bahan <ryanbahan@Ryans-MacBook-Pro-2.local>
2022-09-17 16:27:30 -0600

    wip

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+import introspectionQuery from '../graphql.schema.json'
+import { buildClientSchema, printSchema, Source, buildSchema, IntrospectionQuery, print } from 'graphql'
+
+import {
+  generateMutations,
+} from "./generate-query";
+
+function App() {
+  const builtSchema = buildClientSchema(introspectionQuery as IntrospectionQuery);
+  const schemaLanguage = printSchema(builtSchema);
+  const source = new Source(schemaLanguage);
+  const validSchema = buildSchema(source, { assumeValidSDL: true });
+
+  const configuration = {
+    depthProbability: 0.5,
+    breadthProbability: 0.5,
+    providePlaceholders: true,
+    maxDepth: 250,
+    ignoreOptionalArguments: true,
+  };
+
+  const { mutationDocument, variableValues } = generateMutations(
+    validSchema,
+    configuration
+  );
+
+  // console.log('printed', print(mutationDocument))
+  // console.log({mutationDocument, variableValues})
+
+
+  return (
+    <div className="App">
+      <p>hello world</p>
+    </div>
+  )
+}
+
+export default App
